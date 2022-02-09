e579a3c28fac53c47f7006cc8eff942b5c7745b5
Almog Wertzberger <87434394+AlmogWer@users.noreply.github.com>
2022-02-09 17:04:35 +0200

    Initial commit

---
+++ src/test/java/com/luv2code/mycoolapp/AppTest.java
@@ -0,0 +1,18 @@
+package com.luv2code.mycoolapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
